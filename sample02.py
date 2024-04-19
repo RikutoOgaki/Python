fff12dd7edc1ea900bd6234eefa83a0fa2139d59
Rikuto <orikutobasuke@gmail.com>
2024-04-19 14:53:03 +0900

    sample02編集

--- sample02.py
+++ sample02.py
@@ -1,5 +1,12 @@
 # 初めてのスクレイピング
 import requests
-url = "https://example.com"
+from bs4 import BeautifulSoup
+
+
+url = "https://www.yahoo.co.jp/"
 response = requests.get(url)
 html = response.text
+
+data =  BeautifulSoup(html , "html.parser")
+
+print(data)
